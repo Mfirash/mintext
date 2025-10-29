2dc62b17fbcc0b4f2c16acd616f3dbe2fde401c7
Mfirash <mfirash2013@gmail.com>
2025-10-29 14:05:34 +0800

    Simplify paths in auto.bat for compilation

--- src/auto.bat
+++ src/auto.bat
@@ -1,1 +1,1 @@
-E:\msys2\mingw64\bin\g++.exe -fdiagnostics-color=always -g "C:\Users\PC\Desktop\FIRASH\New folder (2)\mintext\src\New folder\mintext.cpp" "C:\Users\PC\Desktop\FIRASH\New folder (2)\mintext\src\New folder\mintextcore.cpp" -o "C:\Users\PC\Desktop\FIRASH\New folder (2)\mintext\src\New folder\mintext.exe"
+g++.exe -fdiagnostics-color=always -g "mintext.cpp" "mintextcore.cpp" -o "mintext.exe"
